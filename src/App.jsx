bb47f28da2c4a7a67aa4af9d3722fed3720c18cd
Param Pragyan <parampragyan@gmail.com>
2023-08-30 11:42:41 +0530

    added the required files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Split1 from "../src/Components/SplitImg1"
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Split1/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/SplitImg1.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import img1 from "../assets/img1.jpg"
+
+const SplitImg1 = () => {
+  return (
+    <div>
+        <div class="card">
+  <div className="img w-[400px] h-[400px]" >
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    
+  </div>
+  <div class="content">
+    <h2>STUDIO SHAPE & SCAPE</h2>
+    
+  </div>
+</div>
+
+    </div>
+  )
+}
+
+export default SplitImg1
